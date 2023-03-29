60888ae6eb84ba845d40d11ced5ff176d0bc970e
burnout131 <marcrg07@gmail.com>
2023-03-29 13:02:28 +0200

    Cambios en DBConnect, MainApp y Tienda

--- ta18-c2-team02-conexion-java-mysql/src/connectionUtils/DBConnect.java
+++ ta18-c2-team02-conexion-java-mysql/src/connectionUtils/DBConnect.java
@@ -3,16 +3,52 @@ package connectionUtils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
 
 public class DBConnect {
-	public void Connect() {
+	Connection connection;
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void connect() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.68.108:3306?useTimezone=true&server=UTC", "root", "");
+			connection = DriverManager.getConnection("jdbc:mysql://192.168.68.108:3306?useTimezone=true&server=UTC",
+					"root", "");
 			System.out.println("Connected!");
 		} catch (SQLException | ClassNotFoundException ex) {
 			System.out.println("Cannot connect to DB");
 			System.out.println(ex);
+		}
+	}
+
+	public void closeConnection() {
+		try {
+			connection.close();
+			JOptionPane.showMessageDialog(null, "Se ha finalizado la conexión con el servidor");
+		} catch (SQLException ex) {
+			Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public void createDB(String name) {
+		try {
+			String QueryDrop = "DROP DATABASE IF EXISTS" + name;
+			String Query = "CREATE DATABASE " + name;
+			Statement st = connection.createStatement();
+			st.executeUpdate(QueryDrop);
+			st.executeUpdate(Query);
+			closeConnection();
+			connect();
+			JOptionPane.showMessageDialog(null, "Se ha creado la base de datos " + name + " de forma exito");
+		} catch (SQLException ex) {
+			Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 }
--- ta18-c2-team02-conexion-java-mysql/src/ejercicio01/Tienda.java
+++ ta18-c2-team02-conexion-java-mysql/src/ejercicio01/Tienda.java
@@ -2,11 +2,38 @@
  * 
  */
 package ejercicio01;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import connectionUtils.DBConnect;
 
 /**
  * @author team02 (Andrea, Marc Rojo y Paul)
  *
  */
 public class Tienda {
+	private DBConnect conn = new DBConnect();
+	private String db;
+	
+	public void createDatabase() {
+		db = "Tienda";
+		conn.createDB(db);
+	}
+	
+	public void createTable(String query) {
+		
+	    try {
+	        String Querydb = "USE " + db + ";";
+	        Statement stdb = conn.getConnection().createStatement();
+	        stdb.executeUpdate(Querydb);
 
+	        
+	        Statement st= conn.getConnection().createStatement();
+	        st.executeUpdate(query);
+	        System.out.println("Tabla creada con exito!");
+	    } catch (SQLException ex){
+	        System.out.println(ex.getMessage());
+	        System.out.println("Error crando tabla.");
+	    }
+	}
 }
