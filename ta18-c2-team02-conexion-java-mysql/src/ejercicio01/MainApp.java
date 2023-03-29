eff5c021cd9b2ea18800ba17d39a3a3e15218d39
Paul S <62121921+paul-23@users.noreply.github.com>
2023-03-29 10:48:49 +0200

    Creacion primeras clases

---
+++ ta18-c2-team02-conexion-java-mysql/src/ejercicio01/MainApp.java
@@ -0,0 +1,10 @@
+package ejercicio01;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Tienda tiendaInformatica = new Tienda();
+		
+	}
+
+}
---
+++ ta18-c2-team02-conexion-java-mysql/src/ejercicio01/Tienda.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package ejercicio01;
+
+/**
+ * @author team02 (Andrea, Marc Rojo y Paul)
+ *
+ */
+public class Tienda {
+
+}
