75c2efc2ea47cca39b4f3caeb145a77e864cc138
Paul S <62121921+paul-23@users.noreply.github.com>
2023-03-30 00:09:54 +0200

    Finalizacion ejercicio 01

--- ta18-c2-team02-conexion-java-mysql/src/ejercicio01/MainApp.java
+++ ta18-c2-team02-conexion-java-mysql/src/ejercicio01/MainApp.java
@@ -17,7 +17,6 @@ public class MainApp {
 				+ "codigo INT NOT NULL,"
 				+ "nombre NVARCHAR(100),"
 				+ "precio INT,"
-				+ "fabricante INT,"
 				+ "codigo_fabricantes INT NOT NULL,"
 				+ "PRIMARY KEY (codigo),"
 				+ "FOREIGN KEY (codigo_fabricantes)"
@@ -27,11 +26,17 @@ public class MainApp {
 		
 		String insert_fabricantes = "INSERT INTO fabricantes (Nombre) VALUE (\"Fabricante 1\"), (\"Fabricante 2\"), (\"Fabricante 3\"), (\"Fabricante 4\"), (\"Fabricante 5\");";
 		
-		String insert_articulos = "";
+		String insert_articulos = "INSERT INTO articulos (codigo, nombre, precio, codigo_fabricantes) VALUES "
+				+ "(1, \"Articulo 1\", 35, 1),"
+				+ "(2, \"Articulo 2\", 10, 2),"
+				+ "(3, \"Articulo 3\", 19, 3),"
+				+ "(4, \"Articulo 4\", 12, 4),"
+				+ "(5, \"Articulo 5\", 30, 5);";
 		
 		conn.createTable(db, fabricantes); // Creamos la tabla de fabricantes
 		conn.createTable(db, articulos); // Creamos la tabla de articulos
 		conn.insertData(db, insert_fabricantes); // Insertamos valores en la tabla de fabricantes
+		conn.insertData(db, insert_articulos); // Insertamos valores en la tabla de articulos
 
 	}
 
